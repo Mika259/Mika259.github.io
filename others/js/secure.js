4c3459c8d038068601ad9eb17a6e954956e5dbdf
M1K259 <87774553+Mika259@users.noreply.github.com>
2025-02-05 06:10:59 +0800

    Update secure.js

--- others/js/secure.js
+++ others/js/secure.js
@@ -1,5 +1,4 @@
-
-        if(!sessionStorage.getItem('alert_shown')){
-            alert("Website Secured By Snypenz Technology 2025!");
-            sessionStorage.setItem('alert_shown','true')
-        }
+if (!sessionStorage.getItem('alert_shown')){
+    window.alert("Website Secured By Snypenz Technology 2025!");
+    sessionStorage.setItem('alert_shown','true');
+}
