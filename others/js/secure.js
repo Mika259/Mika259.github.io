9ff0b90ac7f67ad863f68d47c7c927f9d814608a
M1K259 <87774553+Mika259@users.noreply.github.com>
2025-02-05 06:06:21 +0800

    Create secure.js

---
+++ others/js/secure.js
@@ -0,0 +1,5 @@
+
+        if(!sessionStorage.getItem('alert_shown')){
+            alert("Website Secured By Snypenz Technology 2025!");
+            sessionStorage.setItem('alert_shown','true')
+        }
